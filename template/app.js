05a229248a217175d6cfdb21c0d9869f1881bd3c
nana <nykim@nykim.net>
2021-01-16 22:13:16 +0900

    gulp 내 ejs 테스트

--- template/app.js
+++ template/app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var ejs = require('ejs');
 var exphbs = require("express-handlebars");
+const bodyParser = require('body-parser');
+var fs   = require('fs');
+var data = {} // put your data here.
+
 
 var app = express();
 var PORT = 4000;
@@ -10,12 +14,23 @@ var PORT = 4000;
 // app.set("view engine", "handlebars");
 
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/app/src'));
 app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/app/src/views'));
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static(path.join(__dirname, '/app/dist/views')));
+
+// app.use('/static', express.static('public'));
+
+
+// var template = fs.readFileSync(path.join(__dirname, '/app/src/views/map.ejs'), 'utf-8');
+// var html     = ejs.render ( template , data );
+
 
 app.get("/", (req, res) => {
-    res.status(200).render("index");
+    // fs.writeFileSync("./app/dist/views/map.html", html, 'utf8');
+    // console.log(html);
+    res.status(200).render("map");
 });
 
 app.listen(PORT, function() {
--- template/gulpfile.js
+++ template/gulpfile.js
@@ -8,7 +8,12 @@ var headerComment = require('gulp-header-comment');
 var gulpEjsMonster = require('gulp-ejs-monster');
 var beautify = require('gulp-beautify');
 var gulpIgnore = require('gulp-ignore');
+var fileinclude = require("gulp-file-include");
 var del = require("del");
+var ejs = require('gulp-ejs');
+var pug = require('gulp-pug');
+var log = require('fancy-log');
+const rename = require('gulp-rename');
 
 global.project = require('./package.json');
 sass.compiler = require('node-sass');
@@ -18,7 +23,10 @@ var paths = project.paths;
 
 var files = {
   css: 'css/**/*.scss',
-  html: 'views/**/*.ejs'
+  ejs: 'views/**/*.ejs',
+  pug: 'views/**/*.pug',
+  html: 'views/**/*.html',
+  js: 'scripts/**/*.js'
 }
 
 var sync = () => {
@@ -40,7 +48,7 @@ var styleSheet = () => {
   return gulp
     .src(files.css, {
       cwd: path.resolve(__dirname, paths.src),
-      since: gulp.lastRun(styleSheet)
+      // since: gulp.lastRun(styleSheet)
       // sourcemaps: true
     })
     .pipe(sass(opts.sass)).on('error', sass.logError)
@@ -50,40 +58,86 @@ var styleSheet = () => {
     .pipe(browserSync.stream());
 };
 
-// var views = () => {
-//   return gulp
-//     .src('views/**/[^_]*.ejs', {
-//       cwd: path.resolve(__dirname, paths.src),
-//       since: gulp.lastRun(views)
-//     })
-//     .pipe(gulp.dest(path.resolve(__dirname, paths.dist)))
-//     .pipe(browserSync.stream());
-// };
+var views = () => {
+  return gulp
+    .src(files.html, {
+      cwd: path.resolve(__dirname, paths.dist)
+    })
+    // .pipe(fileinclude({
+    //   prefix: "@@",
+    //   basepath: "@file",
+    //   context: { "id": "" },
+    // }))
+    .pipe(beautify.html({
+      indent_size: 2,
+      preserve_newlines: false
+    }))
+    .pipe(gulp.dest(path.resolve(__dirname, paths.dist)))
+    .pipe(browserSync.stream());
+};
 
 var ejs = () => {
   return gulp
-  .src(files.html, {
+  .src(files.ejs, {
     cwd: path.resolve(__dirname, paths.src),
     // since: gulp.lastRun(views)
   })
   .pipe(gulpIgnore.exclude('_*/**.ejs'))
   .pipe(gulpEjsMonster())
-  // ! 별도 태스크로 분리
-  // .pipe(beautify.html({
-  //   indent_size: 2,
-  //   preserve_newlines: false
-  // }))
-  .pipe(gulp.dest(path.resolve(__dirname, paths.dist+'/views')))
+  .pipe(rename({ extname: '.html' }))
+  .pipe(gulp.dest(path.resolve(__dirname, paths.dist, 'views')))
   .pipe(browserSync.stream());
 }
 
-var views = () => {
+// var pug = () => {
+//   return gulp
+//     .src(files.pug, {
+//       cwd: path.resolve(__dirname, paths.src),
+//     })
+//     // .pipe(pug({ pretty: true }))
+//     .pipe(gulp.dest(path.resolve(__dirname, paths.dist, 'views')))
+//     .pipe(browserSync.stream());
+// }
 
+
+var html = () => {
+  return gulp
+    .src(files.html, {
+      cwd: path.resolve(__dirname, paths.src)
+    })
+    .pipe(fileinclude({
+      prefix: "@@",
+      basepath: "@file",
+      context: { "id": "" },
+    }))
+    // .pipe(beautify.html({
+    //   indent_size: 2,
+    //   preserve_newlines: false
+    // }))
+    .pipe(gulp.dest(path.resolve(__dirname, paths.dist)))
+    .pipe(browserSync.stream());
+};
+
+var scripts = () => {
+  var opts = {
+    header: comment(project)
+  };
+  return gulp
+  .src(files.js, {
+    cwd: path.resolve(__dirname, paths.src),
+    // since: gulp.lastRun(scripts)
+  })
+  // TODO: 압축 옵션 추가하기
+  .pipe(headerComment(opts.header))
+  .pipe(gulp.dest(path.resolve(__dirname, paths.dist, 'scripts')))
+  .pipe(browserSync.stream());
 }
 
 var watch = () => {
-  gulp.watch(path.resolve(__dirname, paths.src, files.css), { interval: 500 }, styleSheet);
-  gulp.watch(path.resolve(__dirname, paths.src, files.html), ejs);
+  // gulp.watch(path.resolve(__dirname, paths.src, files.ejs), ejs);
+  gulp.watch(path.resolve(__dirname, paths.src, files.html), html);
+  gulp.watch(path.resolve(__dirname, paths.src, files.css), styleSheet);
+  gulp.watch(path.resolve(__dirname, paths.src, files.js), scripts);
 }
 
 function clean() {
@@ -98,8 +152,10 @@ var tasks = gulp.series(
   clean,
   gulp.parallel(
     sync,
-    ejs,
+    // ejs,
+    html,
     styleSheet,
+    scripts,
     watch
   )
 );
